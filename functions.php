864975469d51be47f820c9d84e9fbcff00026472
Ryan Valizan <ryan@vnetkc.com>
2020-07-12 23:15:39 +0000

    :construction: finishing functions part 6 ep

--- functions.php
+++ functions.php
@@ -10,3 +10,69 @@ function hotMama() {
 
 
 <h1><?php hotMama();?></h1>
+
+
+
+<?php
+
+
+/*
+
+tutorial on youTube @
+https://youtu.be/ApP1rgZ9OlY
+Part 6
+
+*/
+
+function simplyFunction() {
+    echo "Hello World";
+}
+
+
+function sayHello ($name = 'World') {
+    echo "Hello $name<br>";
+}
+
+
+
+
+sayHello('Friend');
+
+
+function addNumbers($num1, $num2) {
+    return $num1 + $num2;
+}
+
+function myAge($yearBorn, $thisYear) {
+    return $thisYear - $yearBorn;
+}
+
+echo 'What\'s 19 + 85? ' . addNumbers(19,85) . '<br>';
+
+echo 'How old are you turning this year? ' . myAge(1985, 2020) . '<br>';
+
+
+
+$myNum = 10;
+
+
+function addFive($num) {
+    $num += 5;
+}
+
+
+function addTen(&$num) {
+    $num += 10;
+}
+
+
+addFive($myNum);
+
+
+echo "Value: $myNum<br>";
+
+addTen($myNum);
+
+echo "Value: $myNum<br>";
+
+?>
