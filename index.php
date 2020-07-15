b8ff44c9f7463e322074635b772e96a0773c217e
Ryan Valizan <ryan@vnetkc.com>
2020-07-15 02:21:52 +0000

    :lipstick: added some BS4 tweaks

--- index.php
+++ index.php
@@ -1,25 +1,18 @@
 <?php
-/*
-Developer: Ryan Valizan
-*/
-
-echo "<h1>You did it!</h1>";
-
-echo "You have a working dev lab and its linked to github, yay!";
-
-// testing wakatime
-// wakatime picked up project as html
-// wakatime now picking up project as lrnlab because that is git repo parent dir
-
-// customize where you are going to send visitors or yourself
-$thisPage = 'index.php';
-
-function test() {
-    echo "error again";
-}
+include ('server-info.php');
 ?>
-
-<?php // maybe will pretty this up one day ?>
-<p>
-    <button><a href="<?php echo $thisPage;?>">What I worked on last</a></button>
-</p>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>System Info</title>
+        <link rel="stylesheet" href="css/style.min.css">
+    </head>
+    <body>
+        
+    </body>
+    <footer>
+        <script src="js/bootstrap.min.js" defer></script>
+    </footer>
+</html>
