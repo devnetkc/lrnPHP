5171dbbb961fe7f92acf7e7ebb16dd964e9b4b7e
Ryan Valizan <ryan@vnetkc.com>
2020-07-15 02:42:04 +0000

    :bookmark: End of Part 10

--- index.php
+++ index.php
@@ -11,17 +11,29 @@ include ('server-info.php');
     </head>
     <body>
         <div class="container">
-            <h1>Server &amp; File Info</h1>
-            <?php if ($server): ?>
-                <ul class="list-group">
-                    <?php foreach ($server as $key => $value): ?>
-                        <li class="list-group-item">
-                            <strong><?php echo $key;?>: </strong>
-                            <?php echo $value; ?>
-                        </li>
-                    <?php endforeach; ?>
-                </ul>
-            <?php endif; ?>
+            <h1>$_SERVER Examples</h1>
+                <h2 class="pt-2">Server &amp; File Info</h2>
+                <?php if ($server): ?>
+                    <ul class="list-group">
+                        <?php foreach ($server as $key => $value): ?>
+                            <li class="list-group-item">
+                                <strong><?php echo $key;?>: </strong>
+                                <?php //echo $value; ?>erased for security purposes
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
+                <h2 class="pt-2">Client Info</h2>
+                <?php if ($client): ?>
+                    <ul class="list-group">
+                        <?php foreach ($client as $key => $value): ?>
+                            <li class="list-group-item">
+                                <strong><?php echo $key;?>: </strong>
+                                <?php echo $value; ?>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
         </div>
         
     </body>
