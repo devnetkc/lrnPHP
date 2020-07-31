d2db35d68313b10f5bfe32738960539dc0ff366c
Ryan Valizan <ryan@vnetkc.com>
2020-07-31 21:56:48 +0000

    :building_construction: setup 2 forms 4 get n post

--- includes/part14/part14Header.php
+++ includes/part14/part14Header.php
@@ -2,12 +2,28 @@
 <?php
 
 // my functions of what he is doing in the video
-function isItData( $in ) {
+function isItData( $in, $input ) {
     if( $in ) {
+        $var = 'Your name is ';
+
+        switch ($input) {
+            case 'GET':
+                $var = $_POST['formInputGetName'];
+                break;
+            case 'POST':
+                $var = $_POST['formInputPostName'];
+                break;
+            default:
+                $var = 'Input Not Found';
+        }
+
+        $ret = $var;
+
+
         // that moment you realize you've just spent 30min trying to find the issue and it was the wrong attr tag ;;
-        $ret = 'Your name is ' . $_POST['formInputName']; 
+       // $ret = 'Your name is ' . $_POST['formInputName']; 
     } else {
-        $ret = 'Please enter your name here';
+        $ret = 'Please enter your name';
     }
     return $ret;
 }
--- index.php
+++ index.php
@@ -19,13 +19,48 @@ include ('includes/part14/part14Header.php');
             <p>Project repository and changes tracked on <a rel="nofollow" target="_blank" href="https://github.com/devnetkc/lrnPHP/tree/Part-14-Filters-and-Validation" title="lrnPHP PHP Front to Back repository on GitHub">GitHub.</a></p>
             <div class="row">
                 <div class="col-12-sm col-6-md col-4-lg col-3-xlg">
-                    <div class="container ml-3 my-card">
+                    <div class="container ml-3 my-card mc1">
                         <form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
                             <div class="form-group">
-                                <label for="formInputName">
-                                    Name
+                                <label for="formInputPostName">
+                                    Post Name
                                 </label>
-                                <input type="text" class="form-control" name="formInputName" id="form1InputName" aria-describedby="emailPrivacyNotice" placeholder="<?php echo isItData( filter_has_var(INPUT_POST, 'formInputName' ) ); ?>">
+                                <input type="text" class="form-control" name="formInputPostName" id="formInputPostName" aria-describedby="emailPrivacyNotice" placeholder="<?php echo isItData( filter_has_var(INPUT_POST, 'formInputPostName' ), 'POST' ); ?>">
+                            </div>
+                            <div class="form-group">
+                                <label for="formInputEmail">
+                                    Email address
+                                </label>
+                                <input type="email" class="form-control" id="formInputEmail" aria-describedby="emailPrivacyNotice" placeholder="Enter email">
+                                <small id="emailPrivacyNotice">We'll never share your email with anyone else</small>
+                            </div>
+                            <div class="form-group">
+                                <label for="formInputPassword">
+                                    Password
+                                </label>
+                                <input type="password" class="form-control" id="formInputPassword" placeholder="password">
+                            </div>
+                            <div class="form-group">
+                                <input type="checkbox" class="form-check-input" id="formInputCheck1">
+                                <label class="form-check-label" for="formInputCheck1">Select checkmark value</label>
+                            </div>
+                            <button type="submit" class="btn btn-secondary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+                <div class ="col-12-sm col-6-md col-8-lg col-3-xlg">
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12-sm col-6-md col-4-lg col-3-xlg">
+                    <div class="container ml-3 my-card mc2">
+                        <form method="GET" action="<?php echo $_SERVER['PHP_SELF'];?>">
+                            <div class="form-group">
+                                <label for="formInputGetName">
+                                    Get Name
+                                </label>
+                                <input type="text" class="form-control" name="formInputGetName" id="formInputGetName" aria-describedby="emailPrivacyNotice" placeholder="<?php echo isItData( filter_has_var(INPUT_GET, 'formInputGetName' ), 'GET' ); ?>">
                             </div>
                             <div class="form-group">
                                 <label for="formInputEmail">
@@ -59,3 +94,5 @@ include ('includes/part14/part14Header.php');
         <script src="js/bootstrap.min.js" defer></script>
     </footer>
 </html>
+
+
