3e838f324e07f13f1c382c5b8e5eaa6d0399adef
Ryan Valizan <ryan@vnetkc.com>
2020-08-02 13:45:08 +0000

    :wrench::bug: fixing BS4 dep issues

--- js/config.js
+++ js/config.js
@@ -14,17 +14,39 @@ requirejs.config({
             'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min',
             'node_modules/jquery/dist/jquery.min'
         ],
-        initBootstrap: 'customScripts/initBootstrap',
+        //initBootstrap: 'customScripts/initBootstrap',
         bootstrap: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min',
         popper: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min',
-        methods: 'customScripts/methods'
+        methods: 'customScripts/methods',
+        gascript: 'https://www.googletagmanager.com/gtag/js?id=G-WPX5YZCJGS'
     },
 
     // These are legacy methods to call which will require a browser global 
     // as their dependencies are not defined via define()
     shim: {
-        'bootstrap': [
-            'jquery'
-        ]
+        'popper': {
+            'deps': ['jquery'],
+            'exports': ['Popper']
+        },
+        'bootstrap': {
+            'deps': [
+                'jquery',
+                'popper'
+            ]
+        },
+        'methods': {
+            'deps': [
+                'jquery',
+                'popper',
+                'bootstrap'
+            ]
+        }
+    },
+    
+    // because bootstrap 4 uses a weird popper.js name, we will associate our popper with popper.js calls from BS4
+    map: {
+        '*': {
+            'popper.js': 'popper'
+        }
     }
 });
