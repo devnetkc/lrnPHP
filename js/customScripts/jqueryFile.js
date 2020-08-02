47f2332f3e14b6d05f276fac148a10bf534cff1e
Ryan Valizan <ryan@vnetkc.com>
2020-08-02 13:51:47 +0000

    :zap: setup dep & req in cfg/init file

--- js/customScripts/jqueryFile.js
+++ js/customScripts/jqueryFile.js
@@ -1,11 +1,12 @@
 // put in custom jquery functions here
 
 define ([
-    'jquery',
-    'initBootstrap',
+    // These first two options are no longer needed because they were added as dependencies for methods in the config.js file
+        // 'jquery',
+        // 'bootstrap',
     'methods'
-    ], function (jQuery, methods) {
+    ], function ($, methods) {
         // use custom jQuery 
         // with custom methods for jQuery here
-        console.log('initBootstrap was executed with jquery and methods. bs4 should be available now as well');
+        //console.log('initBootstrap was executed with jquery and methods. bs4 should be available now as well');
     });
--- js/init.js
+++ js/init.js
@@ -1,3 +1,23 @@
 // initialize page load by requiring your js files here
 
-require(['customScripts/jqueryFile']);
+require(['config'], function(){
+    // add all other required scripts here
+        fs = '/';
+        csDir = 'customScripts';
+        csDirFs = csDir + fs;
+
+        csList = [
+            csDirFs + 'gtag',
+            csDirFs + 'jqueryFile'    
+        ];
+
+
+        function loadRequirements (csFile, index) {
+            require([csFile]);
+        }
+
+        // Now that the array is set with our files, lets add them as requirements
+        csList.forEach(loadRequirements);
+
+});
+
