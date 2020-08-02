05e0cb7647db2f6aacd4e6ff8a7e26fe8d628228
Ryan Valizan <ryan@vnetkc.com>
2020-08-02 15:05:41 +0000

    :bento: adding meta tag info

---
+++ includes/metaTags.php
@@ -0,0 +1,25 @@
+
+<?php
+
+$mtitle = 'Visitor Log Side Project | Part 14 -- Filters &amp; Validation';
+$mdesc = 'Still a lot of unrelated changes to the tutorial taking place right now.  Custom msg model work can begin now that requireJS is implimented.';
+$mimg = 'images/shot-mini-lrnlab.jpeg';
+$murl = 'https://lrnlab.vnetkc.com/';
+
+?>
+
+<title><?php echo $mtitle; ?></title>
+<meta name="description" content="<?php echo $mdesc; ?>">
+<meta property="og:title" content="<?php echo $mtitle; ?>">
+<meta property="og:description" content="<?php echo $mdesc; ?>">
+<meta property="og:image" content="<?php echo $mimg; ?>">
+<meta property="og:url" content="<?php echo $murl; ?>">
+<meta name="twitter:card" content="summary_large_image">
+
+<!-- Touch Icons - iOS and Android 2.1+ 152x152 pixels in size. --> 
+<link rel="apple-touch-icon-precomposed" href="images/v_network_icon_152x152.png">
+
+<!-- Firefox, Chrome, Safari, IE 11+ and Opera. 96x96 pixels in size. -->
+<link rel="icon" href="images/v_network_icon_96x96.png">
+
+
--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include ('includes/functions.php');
 include ('includes/part11/server-info.php');
 
 include ('includes/part14/part14Header.php');
-   
+  
 
 ?>
 <!DOCTYPE html>
@@ -11,16 +11,16 @@ include ('includes/part14/part14Header.php');
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Part 14 -- Filters &amp; Validation</title>
         <link rel="stylesheet" href="css/style.min.css">
+        <?php include ( 'includes/metaTags.php' ); ?>
     </head>
     <body>
         <div class="container">
-            <h1>Filters &amp; Validation</h1>
+            <h1>Visitor Log Side Project | Part 14 -- Filters &amp; Validation'</h1>
             <p>Project repository and changes tracked on <a rel="nofollow" target="_blank" href="https://github.com/devnetkc/lrnPHP/tree/add-require-js" title="lrnPHP PHP Front to Back repository on GitHub">GitHub.</a></p>
             <h2 class="h3">The name fields are working again.</h2> 
             <p>Still a lot of unrelated changes to the tutorial taking place right now.</p> 
-            <p>requireJS is implimented now, and custom work for message models can begin.</p>
+            <p>Custom msg model work can begin now that requireJS is implimented.</p>
             <p>Return soon, I'm working on a visitor log!</p> 
             <div class="row">
                 <div class="col-12-sm col-6-md">
