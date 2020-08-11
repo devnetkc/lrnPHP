41601a98b2c44f9642460e7abfa17461d21f9057
Ryan Valizan <ryan@vnetkc.com>
2020-08-11 02:36:01 +0000

    :art: breakup segments better more visitor log wrk

--- index.php
+++ index.php
@@ -16,15 +16,64 @@ include ('includes/part14/part14Header.php');
     </head>
     <body>
         <div class="container">
-            <h1>Visitor Log Side Project | Part 14 -- Filters &amp; Validation'</h1>
+            <h1>Visitor Log Side Project'</h1>
             <p>Project repository and changes tracked on <a rel="nofollow" target="_blank" href="https://github.com/devnetkc/lrnPHP/tree/add-require-js" title="lrnPHP PHP Front to Back repository on GitHub">GitHub.</a></p>
             <h2 class="h3">The name fields are working again.</h2> 
             <p>Still a lot of unrelated changes to the tutorial taking place right now.</p> 
             <p>Custom msg model work can begin now that requireJS is implimented.</p>
             <p>Return soon, I'm working on a visitor log!</p> 
+            <div class="row mb-3">
+                <div class="col-12 col-md-1 col-lg-2 col-xl-3"></div>
+                <div class="col-12 col-md-10 col-lg-8 col-xl-6">
+                    <div class="d-flex justify-content-center my-card mc1">
+
+                        <form method="POST" action="includes/visitor-log/addRecord.php">
+                            
+                                        <div class="d-flex flex-row-reverse align-items-center">
+                                            <div class="pl-2">
+                                                <button type="submit" class="btn btn-secondary">Sign</button>
+                                            </div>
+                                            <div class="pl-2 pr-2 visitorLogName-min-len">
+                                                <input type="text" class="form-control" name="visitorLogName" id="visitorLogName" aria-describedby="emailPrivacyNotice" placeholder="Welcome Visitor, mark your name here.">
+                                            </div>
+                                            <div class="pr-2">
+                                                <label for="visitorLogName" class="mb-0">
+                                                    Visitor Log
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                                            <input type="text" name="b_cc3cc6300ab9753ebb2e03df8_92b9c97470" tabindex="-1" value="">
+                                        </div>
+
+                        </form>
+
+                    </div>
+                </div>
+                <div class="col-12 col-md-1 col-lg-2 col-xl-3"></div>
+            </div>
+            <hr />
+            <div class="row mb-3  lessonHeader">
+                <div class="container">
+                    <div class="d-flex justify-content-between">
+                        <div class="col-10">
+                            <h2 class="align-middle">Part 14 -- Filters &amp; Validation</h2>
+                        </div>
+                        <div class="col-2">
+                            <img class="construction-image" src="images/working-on-it.gif" alt="construction sign to signify work in progress below">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col"><hr class="constructionBreak"></div>
+                <div class="col-auto">Entering Lab Below</div>
+                <div class="col"><hr class="constructionBreak"></div>
+            </div>
             <div class="row">
                 <div class="col-12-sm col-6-md">
-                    <div class="container my-card mc1 ml-3 mc1-ml-3">
+                    <div class="container my-card mc2 ml-3 mc2-ml-3">
+
                         <form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
                             <div class="form-group">
                                 <label for="formInputPostName">
@@ -51,12 +100,14 @@ include ('includes/part14/part14Header.php');
                             </div>
                             <button type="submit" class="btn btn-secondary">Submit</button>
                         </form>
+
                     </div>
                 </div>
 
                         
                 <div class ="col-12-sm col-6-md">
-                    <div class="container ml-3 my-card mc2">
+                    <div class="container ml-3 my-card mc3">
+
                         <form method="GET" action="<?php echo $_SERVER['PHP_SELF'];?>">
                             <div class="form-group">
                                 <label for="formInputGetName">
@@ -83,11 +134,12 @@ include ('includes/part14/part14Header.php');
                             </div>
                             <button type="submit" class="btn btn-secondary">Submit</button>
                         </form>
+                        
                     </div>
                 
 
                 </div>
-            </div>
+            </hr>
             <div class="row">
                 <div class="col-12-sm col-6-md col-4-lg col-3-xlg"></div>
                 <div class ="col-12-sm col-6-md col-8-lg col-3-xlg">
